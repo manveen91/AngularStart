867fbd298e7340e6b1b73c13ef4559d4e244a909
Manveen Randhawa <mrandhawa@fwdco.com>
2014-07-02 15:13:31 -0400

    Added review submission functionality

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var app = angular.module('store', []);
     return this.tab === checkTab;
 	};
   });
+  app.controller("ReviewController", function(){
+    this.review = {};
+	this.addReview = function(product){
+	  product.reviews.push(this.review);
+	  };
+  });
  var gems = [
    {
    name: 'Manveen',
@@ -20,8 +26,19 @@ var app = angular.module('store', []);
    canPurchase: false,
    images: [
    { full: 'https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120'
-   }]
-   },
+   }],
+   reviews: [ 
+   {
+    stars: 5,
+	body: "It was Wonderful experience to work with You!",
+	author: "Frank@thomson.com"
+	},
+	{
+    stars: 4.5,
+	body: "Supporting, Intellectual, Intelligent, Helping person to work with",
+	author: "George@russel.com"
+	}]
+	 },
    {
    name: 'Deep',
    price: 60536.87,
@@ -29,7 +46,18 @@ var app = angular.module('store', []);
    canPurchase: false,
    images: [
    { full: 'https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120'
-   }]
+   }],
+   reviews: [ 
+   {
+    stars: 3,
+	body: "Average Experience!",
+	author: "Frank@thomson.com"
+	},
+	{
+    stars: 4,
+	body: "Awsum person to Interact with.. !",
+	author: "George@russel.com"
+	}]
    }
    ];
  })();
